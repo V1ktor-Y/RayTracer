b3401c8dfa506a1fe7986af403be176c0f71a02f
Viktor <viktoryanev222@gmail.com>
2024-12-26 15:18:04 +0200

    Replaced code in main with header functions

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "color.h"
+#include "vec3.h"
 #include <iostream>
 
 int main() {
@@ -11,15 +13,8 @@ int main() {
               << std::flush;
 
     for (int i = 0; i < IMAGE_WIDTH; i++) {
-      auto r = double(i) / (IMAGE_WIDTH - 1);
-      auto g = double(j) / (IMAGE_HEIGHT - 1);
-      auto b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixelColor = color(double(i), double(j), 0);
+      writeColor(std::cout, normalized_vector(pixelColor));
     }
   }
   std::clog << "\rDone.                 \n";

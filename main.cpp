b061b7944cb57f3a17ea8614a973154393a9e376
Viktor <viktoryanev222@gmail.com>
2024-12-26 12:33:11 +0200

    Basic gradient

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  const int IMAGE_HEIGHT = 256;
+  const int IMAGE_WIDTH = 256;
+
+  std::cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
+
+  for (int j = 0; j < IMAGE_HEIGHT; j++) {
+    for (int i = 0; i < IMAGE_WIDTH; i++) {
+      auto r = double(i) / (IMAGE_WIDTH - 1);
+      auto g = double(j) / (IMAGE_HEIGHT - 1);
+      auto b = 0.0;
+
+      int ir = int(255.999 * r);
+      int ig = int(255.999 * g);
+      int ib = int(255.999 * b);
+
+      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    }
+  }
+
+  return 0;
+}

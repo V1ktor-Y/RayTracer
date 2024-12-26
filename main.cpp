def2b15912b546a6fbde0e3104fc217bdbc20736
Viktor <viktoryanev222@gmail.com>
2024-12-26 15:00:48 +0200

    Added vec3 header file

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main() {
   std::cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
 
   for (int j = 0; j < IMAGE_HEIGHT; j++) {
+    std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' '
+              << std::flush;
+
     for (int i = 0; i < IMAGE_WIDTH; i++) {
       auto r = double(i) / (IMAGE_WIDTH - 1);
       auto g = double(j) / (IMAGE_HEIGHT - 1);
@@ -19,6 +22,6 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
   }
-
+  std::clog << "\rDone.                 \n";
   return 0;
 }
